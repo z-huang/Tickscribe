3ca4f6ebc03a783404957a8fa1d4f9b34250c707
pango0 <tsouoeric@gmail.com>
2025-05-02 21:18:07 +0800

    clean transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,32 +1,11 @@
 import mlx_whisper
 import time
 
-# def transcribe_file(model_path="mlx-community/whisper-small-mlx", verbose=False):
-#     """
-#     Transcribe the audio file using MLX Whisper.
-#     :param file_path: Path to the audio file.
-#     :return: Transcription result.
-#     """
-#     start = time.perf_counter()
-
-#     result = mlx_whisper.transcribe("test.wav", path_or_hf_repo=model_path)
-
-#     end = time.perf_counter()
-#     latency_ms = (end - start) * 1000
-
-#     if verbose:
-#         print(f"[{latency_ms:.2f} ms] {result['text']}")
-#     else:
-#         print(result['text'])
-
-# if __name__ == "__main__":
-#     transcribe_file()
-
 def transcribe_file(file_path, model_path="mlx-community/whisper-small-mlx", verbose=False):
     start = time.perf_counter()
     result = mlx_whisper.transcribe(file_path, path_or_hf_repo=model_path)
     end = time.perf_counter()
     latency_ms = (end - start) * 1000
     if verbose:
-        print(f"[{latency_ms:.2f} ms] {result['text']}")
+        return f"[{latency_ms:.2f} ms] {result['text']}"
     return result['text']
