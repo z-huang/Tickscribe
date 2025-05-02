bcb53488e04b8c121e9cf0ec4ea8266632654203
Zion Huang <zionhuang1107@gmail.com>
2025-05-02 20:29:22 +0800

    feat: update gui

--- gui.py
+++ gui.py
@@ -1,17 +1,9 @@
-import queue
 import sys
 import threading
-
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QListWidgetItem
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
-
-import mlx.core as mx
-import mlx_whisper
-from mlx_whisper.transcribe import ModelHolder
-import numpy as np
-
-from record import LiveRecorder
+from PySide6.QtCore import Qt, QFile, QObject, QThread, QObject, QThread, Signal, Slot, Qt
+from RealtimeSTT import AudioToTextRecorder
 
 
 def load_ui_widget(path: str, parent: QWidget = None) -> QWidget:
@@ -26,55 +18,95 @@ def load_ui_widget(path: str, parent: QWidget = None) -> QWidget:
     return widget
 
 
+class TranscriptionWorker(QObject):
+    stabilized = Signal(str)
+    finished = Signal()
+
+    def __init__(self, recorder):
+        super().__init__()
+        self.recorder = recorder
+        self._running = True
+
+    @Slot()
+    def run(self):
+        while self._running and not self.recorder.is_shut_down:
+            s = self.recorder.text()
+            if s:
+                self.stabilized.emit(s)
+        self.finished.emit()
+
+    def stop(self):
+        self._running = False
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = load_ui_widget('ui/mainwindow.ui', self)
 
-        self.recorder = LiveRecorder()
-        self.audio_thread = None
+        self.recorder = AudioToTextRecorder(
+            model='base',
+            language='en',
+            enable_realtime_transcription=True,
+            on_realtime_transcription_update=self.on_realtime_transcription_update,
+            spinner=False
+        )
+
+        self.chat_lock = threading.Lock()
+
         self.transcribe_thread = None
+        self.transcribe_worker = None
+
         self.ui.recordButton.clicked.connect(self.start_recording)
+        # self.ui.stopButton.clicked.connect(self.stop_recording)
+
+    def on_realtime_transcription_update(self, s):
+        with self.chat_lock:
+            print('Update:', s)
+            if self.ui.chatContent.count() > 0 and \
+                    self.ui.chatContent.item(self.ui.chatContent.count() - 1).data(Qt.UserRole) == 'temp':
+                item = self.ui.chatContent.item(
+                    self.ui.chatContent.count() - 1)
+                item.setText(s)
+            else:
+                item = QListWidgetItem(s)
+                item.setData(Qt.UserRole, 'temp')
+                self.ui.chatContent.addItem(item)
+            self.ui.chatContent.scrollToBottom()
+
+    @Slot(str)
+    def on_realtime_transcription_stabilized(self, s):
+        with self.chat_lock:
+            print('Stable:', s)
+            if self.ui.chatContent.count() > 0 and \
+                    self.ui.chatContent.item(self.ui.chatContent.count() - 1).data(Qt.UserRole) == 'temp':
+                self.ui.chatContent.takeItem(self.ui.chatContent.count() - 1)
+            item = QListWidgetItem(s)
+            self.ui.chatContent.addItem(item)
+            self.ui.chatContent.scrollToBottom()
 
     def start_recording(self):
-        if self.audio_thread is None or not self.audio_thread.is_alive():
-            self.audio_thread = threading.Thread(target=self.audio_thread_func)
-            self.transcribe_thread = threading.Thread(
-                target=self.transcribe_thread_func)
-            self.audio_thread.start()
-            self.transcribe_thread.start()
-
-    def audio_thread_func(self):
-        print("[Info] Audio thread started.")
         self.recorder.start()
-        print("[Info] Audio thread stopped.")
 
-    def transcribe_thread_func(self):
-        print("[Info] Loading model...")
-        holder = ModelHolder.get_model('mlx-community/whisper-small-mlx', mx.float16)
-        try:
-            while True:
-                try:
-                    chunk = self.recorder.queue.get(timeout=1.0)
-                    print('get chunk')
-                except queue.Empty:
-                    if not self.recorder.running:
-                        break
-                    continue
+        self.transcribe_thread = QThread()
+        self.transcribe_worker = TranscriptionWorker(self.recorder)
 
-                chunk = np.squeeze(chunk)
+        self.transcribe_worker.moveToThread(self.transcribe_thread)
+        self.transcribe_worker.stabilized.connect(
+            self.on_realtime_transcription_stabilized)
+        self.transcribe_thread.started.connect(self.transcribe_worker.run)
+        self.transcribe_worker.finished.connect(self.transcribe_thread.quit)
+        self.transcribe_worker.finished.connect(
+            self.transcribe_worker.deleteLater)
+        self.transcribe_thread.finished.connect(
+            self.transcribe_thread.deleteLater)
 
-                result = mlx_whisper.transcribe(
-                    chunk, path_or_hf_repo='mlx-community/whisper-small-mlx', word_timestamps=True
-                )
+        self.transcribe_thread.start()
 
-                if result["segments"]:
-                    text = result["segments"][-1]["text"]
-                    print(text, flush=True)
-                    self.ui.chatContent.addItem(text)
-        except Exception as e:
-            print(e)
-        print("[Info] Transcription thread stopped.")
+    def stop_recording(self):
+        self.recorder.stop()
+        if self.transcribe_worker:
+            self.transcribe_worker.stop()
 
 
 if __name__ == "__main__":
